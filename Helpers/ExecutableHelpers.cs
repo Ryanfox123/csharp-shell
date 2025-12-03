0434c00640e0be7b98b84acac4481cd72c9823f6
Ryan Fox <ryanfox1@hotmail.co.uk>
2025-12-03 21:42:21 +0000

    update

---
+++ Helpers/ExecutableHelpers.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace codecrafters_shell.Helpers
+{
+    public class ExecutableHelpers
+    {
+         public static bool IsExecutable(string path)
+    {
+        if (!File.Exists(path))
+            return false;
+
+        if (OperatingSystem.IsWindows())
+        {
+            return IsWindowsExecutable(path);
+        }
+        else // macOS / Linux / Unix
+        {
+            return IsUnixExecutable(path);
+        }
+    }
+
+    private static bool IsWindowsExecutable(string path)
+    {
+        string ext = Path.GetExtension(path).ToLowerInvariant();
+        return ext is ".exe" or ".bat" or ".cmd" or ".com" or ".ps1";
+    }
+
+    private static bool IsUnixExecutable(string path)
+    {
+        var mode = File.GetUnixFileMode(path);
+
+        return mode.HasFlag(UnixFileMode.UserExecute) ||
+               mode.HasFlag(UnixFileMode.GroupExecute) ||
+               mode.HasFlag(UnixFileMode.OtherExecute);
+    }
+    }
+}
---
+++ src/main.cs
@@ -0,0 +1,98 @@
+
+using codecrafters_shell.Helpers;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+    
+    var pathEnv = System.Environment.GetEnvironmentVariable("PATH");
+    string[] paths = pathEnv.Split(Path.PathSeparator);
+
+    var cmds = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+    {
+        "echo",
+        "type",
+        "exit"
+    };
+     while (true) 
+        {
+        Console.Write("$ ");
+        String? input = Console.ReadLine()?.Trim();
+        string[] parts = input.Split(' ', 2);
+        string command = parts[0];
+        string argument = parts.Length > 1 ? parts[1] : "";
+
+
+        if (string.IsNullOrEmpty(input) || input.ToLower() == "exit")
+        {
+            break;
+        } 
+        else if (command.ToLower() == "echo")
+        {
+            Console.WriteLine($"{argument}");
+        }
+        else if (command.ToLower() == "type")
+        {
+            if (cmds.Contains(argument.ToLower()))
+            {
+                Console.WriteLine($"{argument} is a shell builtin");
+            }
+            else
+            {
+                var isFound = false;
+
+                foreach (var dir in paths)
+                {
+                    var currSearch = Path.Join(dir, argument);
+                    if (File.Exists(currSearch) && ExecutableHelpers.IsExecutable(currSearch))
+                    {
+                        isFound = true;
+                        Console.WriteLine($"{argument} is {currSearch}");
+                        break;
+                    }
+                }
+                if (!isFound)
+                {
+                Console.WriteLine($"{argument}: not found");
+                }
+            }
+        } 
+        else
+        {
+            var isFound = false;
+            foreach (var dir in paths)
+                {
+                    var currSearch = Path.Join(dir, command);
+                    if (File.Exists(currSearch) && ExecutableHelpers.IsExecutable(currSearch))
+                    {
+                        isFound = true;
+                        ProcessStartInfo start = new ProcessStartInfo();
+                        start.Arguments = argument;
+                        start.FileName = command;
+                        start.RedirectStandardOutput = true;
+                        start.RedirectStandardError = true; 
+                        start.UseShellExecute = false;
+                        
+                        using (Process process = new Process())
+                        {
+                            process.StartInfo = start;
+                            process.Start();
+
+                            string output = process.StandardOutput.ReadToEnd();
+
+                            process.WaitForExit();
+
+                            Console.Write(output);
+                        }
+                    }
+                }
+            if (!isFound) 
+            {
+                Console.WriteLine($"{input}: command not found");
+            }
+        }
+        }
+    }
+}
